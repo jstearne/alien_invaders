bc858a465cf6e3e43dfe110e83fd26b06fc9fd47
Jared Stearne <jared.stearne@gmail.com>
2021-10-04 12:52:22 -0700

    Bug fix, cleanup. Was getting attribute error due to indentation of _create_alien.

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,16 +87,29 @@ class AlienInvasion:
     def _create_fleet(self):
         """Creates a fleet of enemy aliens."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Creating the first row of aliens!
-        for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # Number of alien rows that fit on screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Create full fleet of aliens!
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and put it in the row."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
 
     def _update_screen(self):
